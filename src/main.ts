5fd579ce70682d286fbe17a0a7e167b1c4bdfa9d
Jonathan Ardila <mized2009@gmail.com>
2025-05-13 03:28:13 -0500

    add environment configuration and Swagger, yml

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Config de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Epayco API Service')
+    .setDescription('Documentación de la API Service de Epayco')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
